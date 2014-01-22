28e7f430f7d46c0f6e9c3ac22f2541529d82e41c
Treenson <treenson@gmail.com>
2014-01-22 08:05:32 +0000

    get started.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Jchun726::Application.config.secret_key_base = 'ec6dd2fa7bb112ebafb15c73724a31f618cc6c70ef89d813b3f2cbbb3c7cf51d26367ab9aa27faa5ad386c4b2ba628e30f4f96a00f35aa90856dc7984180b4e1'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Jchun726::Application.config.session_store :cookie_store, key: '_jchun726_session'
